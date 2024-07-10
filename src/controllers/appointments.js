6dd55366405c1d62e0f4e9a5bf8ba097dc4c94ef
Alexis Guzman <spartanthelexvro@gmail.com>
2024-07-09 21:37:25 -0600

    Arreglando consulta SQL

--- src/controllers/appointments.js
+++ src/controllers/appointments.js
@@ -40,7 +40,7 @@ exports.getAll = [
     const fecha_actual = req.params.fecha_actual;
     db.query(
       //Mover datos del paciente
-      "SELECT id_cita, nombre, apellidoP, apellidoM, telefono, fecha, horario_inicio FROM pacientes NATURAL JOIN citas NATURAL JOIN horarios_atencion WHERE fecha >= ? GROUP BY id_cita;",
+      "SELECT id_cita, nombre, apellidoP, apellidoM, telefono, fecha, horario_inicio FROM pacientes NATURAL JOIN citas NATURAL JOIN horarios_atencion WHERE fecha >= ? AND id_horario_atencion = id_horario;",
       [fecha_actual],
       async (err, result) => {
         if (err) {
